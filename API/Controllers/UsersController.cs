fd0ebe2b78485102c8d626e65ed487cfb36e6c8a
Stanley <egbestanley84@yahoo.com>
2021-11-03 12:11:12 +0100

    End of Section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Resources;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,23 +15,32 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers {
     [ApiController]
     [Route ("api/[controller]")]
+
+    
+        [Authorize]
     public class UsersController : BaseApiController {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
         private readonly DataContext _context;
-        public UsersController (DataContext context) {
-            _context = context;
+        public UsersController(IUserRepository userRepository, IMapper mapper) {
+            _mapper = mapper;
+            _userRepository = userRepository;
+          
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-            return await _context.Users.ToListAsync();
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
+            var users = await _userRepository.GetMembersAsync();
+
+            return Ok(users);
             
         }
 
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id){
-            return await _context.Users.FindAsync(id);
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username){
+
+            return await _userRepository.GetMemberAsync(username); 
             
         }
     }
---
+++ API/Data/Migrations/20211102125356_ExtendedUserIdentity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class ExtendedUserIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KnownAss",
+                table: "Users",
+                newName: "KnownAs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KnownAs",
+                table: "Users",
+                newName: "KnownAss");
+        }
+    }
+}
